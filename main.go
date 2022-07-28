8a7b42cf2b0036a495cfcac251a76ed4e26e4f07
Jason <jasonelish@gmail.com>
2022-07-28 17:47:06 -0600

    Have quiz working with timer. Quiz will interrupt when timer goes off and display score. Still needs some cleanup but checkpoint for dinner time

--- main.go
+++ main.go
@@ -9,6 +9,7 @@ import (
 	"runtime"
 	"strconv"
 	"strings"
+	"time"
 )
 
 func main() {
@@ -30,46 +31,66 @@ func main() {
 		currentQuestion := QuestionAnswer{Question: line[0], Answer: answer}
 		questionsAndAnswers = append(questionsAndAnswers, currentQuestion)
 	}
-	playGame(questionsAndAnswers)
-}
-
-func playGame(questionsAndAnswers []QuestionAnswer) {
 	reader := bufio.NewReader(os.Stdin)
-	correctAnswers := 0
 	fmt.Println("Are you ready to play? Y or N")
 	response, err := reader.ReadString('\n')
 	if err != nil {
 		log.Fatal("Error reading user input from standard input")
 	}
 	trimResponses(&response)
-
-	if response == "Y" {
-		for _, question := range questionsAndAnswers {
-			fmt.Printf("What is %s", question.Question)
-			response, err = reader.ReadString('\n')
-			if err != nil {
-				fmt.Println("Unable to read user input, assuming incorrect answer")
-			}
-			trimResponses(&response)
-			answer, err := strconv.Atoi(response)
-			if err != nil {
-				fmt.Println("You entered an invalid response, assuming incorrect answer")
-			}
-			if answer == question.Answer {
-				fmt.Println("That is correct")
-				correctAnswers++
-			} else {
-				fmt.Println("That is incorrect")
-			}
+	if response != "Y" {
+		fmt.Println("Exiting application")
+		os.Exit(0)
+	}
+	answerChannel := make(chan int)
+	quitChannel := make(chan string)
+	finalScore := 0
+	go gameTimer(10, quitChannel)
+	go playGame(questionsAndAnswers, reader, answerChannel, quitChannel)
+readChannel:
+	for {
+		select {
+		case <-quitChannel:
+			fmt.Printf("You got %d questions correct", finalScore)
+			break readChannel
+		case <-answerChannel:
+			finalScore++
 		}
-		fmt.Printf("You got %d questions correct", correctAnswers)
 	}
 }
 
+func playGame(questionsAndAnswers []QuestionAnswer, reader *bufio.Reader, answerChannel chan int, quitChannel chan string) {
+
+	for _, question := range questionsAndAnswers {
+		fmt.Printf("What is %s\n", question.Question)
+		response, err := reader.ReadString('\n')
+		if err != nil {
+			fmt.Println("Unable to read user input, assuming incorrect answer")
+		}
+		trimResponses(&response)
+		answer, err := strconv.Atoi(response)
+		if err != nil {
+			fmt.Println("You entered an invalid response, assuming incorrect answer")
+		}
+		if answer == question.Answer {
+			fmt.Println("That is correct")
+			answerChannel <- 1
+		} else {
+			fmt.Println("That is incorrect")
+		}
+	}
+	quitChannel <- "Quiz Completed"
+}
 func trimResponses(response *string) {
 	if runtime.GOOS == "windows" {
 		*response = strings.TrimRight(*response, "\r\n")
 	} else {
 		*response = strings.TrimRight(*response, "\n")
 	}
+}
+
+func gameTimer(timeLimit int, quitChannel chan string) {
+	time.AfterFunc(time.Second*time.Duration(timeLimit), func() {
+		quitChannel <- "Time is Up"
+	})
 }
